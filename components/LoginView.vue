a990b059b0a9bdc8d3dd98c87890c8e1cbe53f8f
Larry W <l422y@l422y.com>
2023-03-24 08:31:26 -0400

    refactor: extract login view

---
+++ components/LoginView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="login">
+    <button class="btn login__with-spotify" @click="startSpotifyAuth">Log in with Spotify</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const startSpotifyAuth = () => {
+  const authUrl = useSpotifyAuthURL()
+  window.location.href = authUrl
+}
+</script>
+
+<style lang="scss" scoped>
+.login {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+
+  &__with-spotify {
+    font-size: 1.2rem;
+    font-weight: 600;
+    padding: 10px 20px;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+    color: #fff;
+    border: none;
+    border-radius: 50px;
+    background-color: #1db954;
+
+    &:hover {
+      background-color: #1ed760;
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,16 @@
 <script lang="ts" setup>
-import PlaylistsView from '@/components/PlaylistsView.vue'
-import PlaylistView from '@/components/PlaylistView.vue'
-import {useAppStore} from "~/stores/app";
+import { useAppStore } from "~/stores/app"
+import LoginView from "~/components/LoginView.vue"
+
 const pv = ref()
 const appStore = useAppStore()
-const tracks = {}
-
-
-const startSpotifyAuth = () => {
-  const authUrl = useSpotifyAuthURL()
-  window.location.href = authUrl
-}
 </script>
 
 <template>
-  <div id="app">
-    <template v-if="appStore.spotifyUser && appStore.spotifyAccessToken">
-      <div :class="{ showPlaylist: appStore.showPlaylist }" class="container">
-        <PlaylistView :showing-playlist="appStore.showPlaylist" :tracks="tracks"/>
-        <PlaylistsView ref="pv"/>
-      </div>
-    </template>
-    <template v-else>
-      <button @click="startSpotifyAuth">Log in with Spotify</button>
-    </template>
-  </div>
+  <ClientOnly>
+    <PlaylistsView v-if="appStore.spotifyUser && appStore.spotifyAccessToken" ref="pv"/>
+    <div v-else>
+      <LoginView/>
+    </div>
+  </ClientOnly>
 </template>
