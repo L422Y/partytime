bb829118a7a18d5d04e488d0fb9c531b5c177052
Larry W <l422y@l422y.com>
2023-03-24 08:38:27 -0400

    refactor: layout

--- components/TrackItem.vue
+++ components/TrackItem.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
-import type { ISpotifyPlaylistTrack } from '@/types/spotify'
-import { computed } from 'vue'
+import type { ISpotifyPlaylistTrack } from "@/types/spotify"
+import { computed } from "vue"
 
 const props = defineProps<{ track: ISpotifyPlaylistTrack }>()
 const track = computed(() => props.track?.track)
@@ -9,14 +9,14 @@ const trackLink = computed(() => track.value?.external_urls.spotify)
 const artistNames = computed(() =>
   track.value?.artists
     .map((artist) => `<a target="_blank" href="${artist.external_urls.spotify}">${artist.name}</a>`)
-    .join(', ')
+    .join(", ")
 )
 </script>
 
 <template>
   <li class="track">
     <a :href="trackLink" class="track__name" target="_blank">{{ trackName }}</a>
-    <div class="track__artists" v-html="artistNames" />
+    <div class="track__artists" v-html="artistNames"/>
     <!--    <a :href="track.external_urls.spotify" class="playlist-view__track-title" target="_blank">{{ track.name }}</a>-->
     <!--        <span v-for="artist in track.track.artists" :key="artist.id" class="artist">-->
     <!--          <a :href="artist.external_urls.spotify" rel="author">{{ artist.name }}</a>-->
@@ -25,11 +25,15 @@ const artistNames = computed(() =>
 </template>
 <style lang="scss">
 .track {
-  padding: 0.5rem 0;
+  padding: 0.5rem 0.5rem 1rem;
   border-bottom: 1px solid #eee;
 
   &::marker {
     font-size: 1.7rem;
+  }
+
+  a {
+    text-decoration: none;
   }
 
   &__name {
