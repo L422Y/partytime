7f5665e8a6fa7f45f20431fcf8984ff5e30e7f09
Larry W <l422y@l422y.com>
2023-03-24 08:29:19 -0400

    chore: refactor store

--- stores/app.ts
+++ stores/app.ts
@@ -1,47 +1,38 @@
-import {defineStore} from 'pinia'
-import {ref} from 'vue'
+import { defineStore } from "pinia"
+import { ref } from "vue"
 
-export const useAppStore = defineStore('appStore', () => {
+export const useAppStore = defineStore("appStore",  () => {
     const showPlaylist = ref(false)
     const currentPlaylist = ref({})
     const playlists = ref({})
-    const spotifyAuthCode = ref('')
+    const spotifyAuthCode = ref("")
     const spotifyUser = ref({})
-    const spotifyAccessToken = ref('')
-    const spotifyRefreshToken = ref('')
-
-    if (process?.client) {
-        const lsPlaylist = localStorage?.getItem('current_playlist')
-        const lsUser = localStorage?.getItem('spotify_user')
-        playlists.value = lsPlaylist && lsPlaylist != 'undefined' ? JSON.parse(lsPlaylist) : {}
-        spotifyUser.value = lsUser && lsUser != 'undefined' ? JSON.parse(lsUser) : {}
-        spotifyAccessToken.value = localStorage?.getItem('spotify_access_token') || ""
-        spotifyRefreshToken.value = localStorage?.getItem('spotify_refresh_token') || ""
-    }
+    const spotifyAccessToken = ref("")
+    const spotifyRefreshToken = ref("")
 
     const setSpotifyUser = (user) => {
-        localStorage?.setItem('spotify_user', JSON.stringify(user))
+        localStorage?.setItem("spotify_user", JSON.stringify(user))
         spotifyUser.value = user
     }
 
     const setCurrentPlaylist = (playlist) => {
-        localStorage?.setItem('current_playlist', JSON.stringify(playlist))
+        localStorage?.setItem("current_playlist", JSON.stringify(playlist))
         currentPlaylist.value = playlist
     }
 
-    const setPlaylists = (playlists) => {
-        localStorage?.setItem('playlists', JSON.stringify(playlists || []))
-        currentPlaylist.value = playlists
+    const setPlaylists = (items) => {
+        localStorage?.setItem("playlists", JSON.stringify(items || []))
+        playlists.value = items
     }
 
     const clearAuthCode = () => {
-        spotifyAuthCode.value = ''
-        localStorage?.removeItem('spotify_auth_code')
+        spotifyAuthCode.value = ""
+        localStorage?.removeItem("spotify_auth_code")
     }
 
     const clearAccessToken = () => {
-        spotifyAccessToken.value = ''
-        localStorage?.removeItem('spotify_access_token')
+        spotifyAccessToken.value = ""
+        localStorage?.removeItem("spotify_access_token")
     }
     const useAccessAuthorizationResponse = (response: {
         access_token: string
@@ -51,16 +42,16 @@ export const useAppStore = defineStore('appStore', () => {
         scope: string
     }) => {
         if (response) {
-            console.log('useAccessAuthorizationResponse', response)
+            console.log("useAccessAuthorizationResponse", response)
             if (response.access_token) {
-                localStorage?.setItem('spotify_access_token', response.access_token)
+                localStorage?.setItem("spotify_access_token", response.access_token)
             }
             if (response.refresh_token) {
-                localStorage?.setItem('spotify_refresh_token', response.refresh_token)
+                localStorage?.setItem("spotify_refresh_token", response.refresh_token)
             }
             localStorage?.setItem(
-                'spotify_refresh_expires',
-                (Date.now() + response.expires_in * 1000).toString()
+                "spotify_refresh_expires",
+                ( Date.now() + response.expires_in * 1000 ).toString()
             )
 
             spotifyAccessToken.value = response.access_token
