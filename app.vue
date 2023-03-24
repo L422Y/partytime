3d78b86dcfa5c71c765e3e0cda130ac37babdfe7
Larry W <l422y@l422y.com>
2023-03-24 09:31:45 -0400

    refactor: attempt to fix process is not defined

--- app.vue
+++ app.vue
@@ -1,21 +1,22 @@
 <template>
-  <NuxtLayout />
+  <NuxtLayout/>
 </template>
 <script lang="ts" setup>
 import { useAppStore } from "~/stores/app"
 
 const appStore = useAppStore()
 
-if (process?.client) {
-    const lsPlaylist = localStorage?.getItem("current_playlist")
-    const lsPlaylists = localStorage?.getItem("playlists")
-    const lsUser = localStorage?.getItem("spotify_user")
+</script>
+<script lang="ts">
+if (process.client) {
+  const lsPlaylist = localStorage?.getItem("current_playlist")
+  const lsPlaylists = localStorage?.getItem("playlists")
+  const lsUser = localStorage?.getItem("spotify_user")
 
-    appStore.$state.playlists = lsPlaylists && lsPlaylists != "undefined" ? JSON.parse(lsPlaylists) : {}
-    appStore.$state.currentPlaylist = lsPlaylist && lsPlaylist != "undefined" ? JSON.parse(lsPlaylist) : {}
-    appStore.$state.spotifyUser = lsUser && lsUser != "undefined" ? JSON.parse(lsUser) : {}
-    appStore.$state.spotifyAccessToken = localStorage?.getItem("spotify_access_token") || ""
-    appStore.$state.spotifyRefreshToken = localStorage?.getItem("spotify_refresh_token") || ""
+  appStore.$state.playlists = lsPlaylists && lsPlaylists != "undefined" ? JSON.parse(lsPlaylists) : {}
+  appStore.$state.currentPlaylist = lsPlaylist && lsPlaylist != "undefined" ? JSON.parse(lsPlaylist) : {}
+  appStore.$state.spotifyUser = lsUser && lsUser != "undefined" ? JSON.parse(lsUser) : {}
+  appStore.$state.spotifyAccessToken = localStorage?.getItem("spotify_access_token") || ""
+  appStore.$state.spotifyRefreshToken = localStorage?.getItem("spotify_refresh_token") || ""
 }
 </script>
-
