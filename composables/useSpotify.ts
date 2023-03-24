9b3b7a26b7a704fdd1178bcc8a6ff8aef73b9508
Larry W <l422y@l422y.com>
2023-03-24 08:33:48 -0400

    chore: fix .env private config

--- composables/useSpotify.ts
+++ composables/useSpotify.ts
@@ -1,128 +1,68 @@
-import {useAppStore} from '@/stores/app'
+import { useAppStore } from "@/stores/app"
+import { useSpotifyAuthRefresh } from "@/composables/useSpotifyAuth"
 
-const useAuthResponse = async (response: any) => {
-    const appStore = useAppStore()
-    if (response.data?.error) {
-        switch (response.data.error) {
-            case 'invalid_grant':
-                // The provided authorization grant (e.g., authorization code, resource owner credentials) or refresh token is invalid, expired, revoked, does not match the redirection URI used in the authorization request, or was issued to another client.
-                appStore.clearAuthCode()
-                break
-            case 'invalid_client':
-                // Client authentication failed (e.g., unknown client, no client authentication included, or unsupported authentication method).
-                break
-            case 'invalid_request':
-                // The request is missing a required parameter, includes an invalid parameter value, includes a parameter more than once, or is otherwise malformed.
-                break
-            case 'unauthorized_client':
-                // The authenticated client is not authorized to use this authorization grant type.
-                break
-            default:
-            // Handle any other errors
+export const useSpotifyAPI = async (path, params, retry = true) => {
+  const appStore = useAppStore()
+  const url = new URL(`https://api.spotify.com/v1${path}`)
+  Object.entries(params).forEach(([key, value]) => url.searchParams.append(key, value))
+
+  const response = await useFetch(url.toString(), {
+    method: "GET",
+    headers: {Authorization: `Bearer ${appStore.spotifyAccessToken}`},
+  }).then(async ({error, data, pending}) => {
+    if (error?.value) {
+      console.error("ERROR: ", error.value.message)
+      if (retry) {
+        // watch(error, async (error) => {
+        if (error.value.statusCode === 401) {
+          await useSpotifyAuthRefresh()
+          await useSpotifyAPI(path, params, false)
         }
-    } else {
-        // Handle the response from your server
-        appStore.useAccessAuthorizationResponse(response)
-        await useGetSpotifyUser()
+        // })
+      }
     }
+    if (data?.value) {
+      console.log({data, error})
+    }
+    if (pending?.value) {
+      console.log({pending})
+    }
+    return {error, data, pending}
+  }).catch((error) => {
+    console.error("ERROR: ", error.message)
+  })
+
+  return response
 }
-export const useGetAccessToken = async (authorizationCode: string) => {
-    console.log('GET ACCESS TOKEN', {authorizationCode})
-    const appStore = useAppStore()
-    if (authorizationCode) {
-        const response = await fetch('/api/token', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                code: authorizationCode,
-            }),
-        })
-        console.log({response})
-        await useAuthResponse(response);
-    } else {
-        // Handle errors, e.g. show an error message or redirect to an error page
-        console.error('Error during authentication:', authorizationCode)
-    }
+export const useSpotifyGetUser = async (userId?: string) => {
+  const appStore = useAppStore()
+  const {data: {value: user}} = await useSpotifyAPI(userId ? `/users/${userId}` : "/me", {})
+  appStore.setSpotifyUser(user)
+  await useSpotifyGetPlaylists()
 }
 
-export const useGetSpotifyUser = async (userId?: string) => {
-    const appStore = useAppStore()
-    await useSpotifyAPI('/me', {}).then((oData) => {
-        console.log({useGetSpotifyUser: oData.data})
-        appStore.setSpotifyUser(oData.data)
+export const useSpotifyGetPlaylists = async () => {
+  const appStore = useAppStore()
+  if (appStore.spotifyAccessToken && appStore.spotifyUser) {
+    return useSpotifyAPI(`/users/${appStore.spotifyUser.id}/playlists`, {
+      limit: 50,
+      offset: 0
+    }).then((response) => {
+      if (response) {
+        appStore.setPlaylists(response.data.value)
+        return response
+      }
     })
+  }
 }
-
-export const useSpotifyAPI = (path, data) => {
-    const appStore = useAppStore()
-    const url = new URL(`https://api.spotify.com/v1${path}`);
-    Object.entries(data).forEach(([key, value]) => url.searchParams.append(key, value));
-
-    return fetch(url, {
-        method: 'GET',
-        headers: {
-            Authorization: `Bearer ${appStore.spotifyAccessToken}`,
-        },
+export const useSpotifyGetPlaylist = async (playlistId: string) => {
+  const appStore = useAppStore()
+  if (appStore.spotifyAccessToken && appStore.spotifyUser) {
+    return useSpotifyAPI(`/playlists/${playlistId}`, {}).then((response) => {
+      if (response) {
+        appStore.setCurrentPlaylist(response?.data?.value)
+        return response
+      }
     })
-
-}
-export const useSpotifyAuthRefresh = async () => {
-    const appStore = useAppStore()
-    appStore.clearAccessToken()
-    if (appStore.spotifyRefreshToken) {
-        await fetch('/api/token', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                refresh_token: appStore.spotifyRefreshToken,
-                grant_type: 'refresh_token',
-            }),
-        })
-            .then(async (response) => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error ${response.status}`);
-                }
-                await useAuthResponse(response.json());
-            })
-            .catch((error) => {
-                console.error('ERROR: ', error.message);
-            });
-
-    }
-}
-
-export const useSpotifyAuthURL = () => {
-    console.log(useRuntimeConfig())
-    const {spotifyClientId, spotifyCallbackURL} = useRuntimeConfig().public
-    const scopes = 'user-read-private user-read-email'
-    console.log({spotifyClientId, spotifyCallbackURL})
-    const authUrl =
-        `https://accounts.spotify.com/authorize?` +
-        `client_id=${spotifyClientId}` +
-        `&response_type=code` +
-        `&redirect_uri=${spotifyCallbackURL}` +
-        `&scope=${scopes}`
-
-    console.log(authUrl)
-    // const left = (window.screen.width / 2) - (500 / 2)
-    // const top = (window.screen.height / 2) - (780 / 2)
-    // window.open(, '_blank', `width=500,height=780,left=${ left },top=${ top }`)
-    return authUrl
-}
-
-export const useSpotifyPlaylistRefresher = async () => {
-    const appStore = useAppStore()
-    if (appStore.spotifyAccessToken && appStore.spotifyUser) {
-        await useSpotifyAPI(`/users/${appStore.spotifyUser.id}/playlists`, {
-            limit: 50,
-            offset: 0
-        }).then((oData: any) => {
-            console.log(oData.data)
-            appStore.setPlaylists(oData.data)
-        })
-    }
+  }
 }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-const privateRuntimeConfig = import("./nuxt.private.config")
 export default defineNuxtConfig({
-    modules: ["@pinia/nuxt"],
-    css: ['@/src/assets/scss/base.scss'],
-    devServer: {
-        port: 5173
+  modules: ["@pinia/nuxt"],
+  css: ["@/src/assets/scss/base.scss"],
+  devServer: {
+    port: 5173
+  },
+  runtimeConfig: {
+    public: {
+      spotifyRedirectURL: "http://localhost:5173/",
+      spotifyCallbackURL: "http://localhost:5173/spotify-callback",
+      spotifyClientId: "e25c017bdd6b476aaece984d99a0fd5c",
     },
-    runtimeConfig: {
-        public: {
-            spotifyRedirectURL: "http://localhost:5173/",
-            spotifyCallbackURL: "http://localhost:5173/spotify-callback",
-            spotifyClientId: "e25c017bdd6b476aaece984d99a0fd5c",
-        },
-        ...privateRuntimeConfig
-    }
+  }
 })
