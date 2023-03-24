670794c4c551074e57a9c903415ff50678a5aef3
Larry W <l422y@l422y.com>
2023-03-24 09:54:57 -0400

    fix: use process.env on server side

--- server/api/token.ts
+++ server/api/token.ts
@@ -1,33 +1,35 @@
 export default defineEventHandler(async (event) => {
-    const {spotifyClientId, spotifyCallbackURL} = useRuntimeConfig().public
-    const {spotifyClientSecret} = useRuntimeConfig()
-    if (spotifyClientId && spotifyClientSecret && spotifyCallbackURL) {
-        const tokenUrl = `https://accounts.spotify.com/api/token`
-        const body = await readBody(event)
-        const requestBody = {
-            grant_type: body?.grant_type || "authorization_code",
-            code: body?.code,
-            redirect_uri: spotifyCallbackURL,
-            client_id: spotifyClientId,
-            client_secret: spotifyClientSecret
-        }
-
-        if (body?.refresh_token) {
-            requestBody.refresh_token = body?.refresh_token
-        }
-
-        const searchBody = new URLSearchParams(requestBody).toString()
-        return await $fetch(tokenUrl, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
-            body: searchBody
-        })
-            .then((response) => response)
-            .catch((error) => {
-                return {error: error}
-            })
-
+  const {spotifyClientId, spotifyCallbackURL} = useRuntimeConfig().public
+  const spotifyClientSecret = process.env.NUXT_SPOTIFY_CLIENT_SECRET
+  if (spotifyClientId && spotifyClientSecret && spotifyCallbackURL) {
+    const tokenUrl = `https://accounts.spotify.com/api/token`
+    const body = await readBody(event)
+    const requestBody = {
+      grant_type: body?.grant_type || "authorization_code",
+      code: body?.code,
+      redirect_uri: spotifyCallbackURL,
+      client_id: spotifyClientId,
+      client_secret: spotifyClientSecret
     }
+
+    if (body?.refresh_token) {
+      requestBody.refresh_token = body?.refresh_token
+    }
+
+    const searchBody = new URLSearchParams(requestBody).toString()
+    return await $fetch(tokenUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: searchBody
+    })
+      .then((response) => response)
+      .catch((error) => {
+        return {error: error}
+      })
+  } else {
+    return {error: "Missing spotify client id or secret", env: process.env}
+  }
+
 })
