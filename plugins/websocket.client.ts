b6b919784fe7eb4bfd1913689177620393a9f0b0
Larry W <l422y@l422y.com>
2023-03-31 17:27:49 -0400

    chore: initial websocket implementation

---
+++ plugins/websocket.client.ts
@@ -0,0 +1,57 @@
+// import { io } from "socket.io-client"
+import { useAppStore } from "~/stores/app"
+// import { computed } from "vue"
+//
+import { io, Socket } from "socket.io-client"
+import { computed } from "vue"
+import { useVotesStore } from "~/stores/votes"
+import { usePlayerStore } from "~/stores/player"
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const appStore = useAppStore()
+  const votesStore = useVotesStore()
+  const playerStore = usePlayerStore()
+
+  let socket: Socket
+  const spotifyUser = computed(() => appStore.spotifyUser)
+  console.log("Websocket Plugin Loaded")
+
+  watch(votesStore, (value) => {
+    if (socket) {
+      socket.emit("votes", value.votes.value)
+    }
+  })
+
+  watch(spotifyUser, (value) => {
+    if (!socket && spotifyUser?.value?.email) {
+      socket = io("http://localhost:5174", {
+        transports: ["websocket"],
+        path: `/${spotifyUser.value.email}/`,
+      })
+      socket.on("connect", () => {
+        console.log("WebSocket Connected", socket.id)
+      })
+      socket.on("connect_error", () => {
+        console.log("WebSocket Disconnected, reconnecting in 1 second...")
+        setTimeout(() => {
+          socket.connect()
+        }, 1000)
+      })
+      socket.on("message", (data: any) => {
+        console.log("message received: %s", data)
+        socket.emit("message", {data})
+      })
+      socket.on("newVote", ({data}) => {
+        console.log("vote received: %s", data)
+        socket.emit("vote", {data})
+      })
+
+      socket.on("votesUpdated", ({data}) => {
+        console.log("votesUpdated", data)
+        if (data?.votes) {
+          votesStore.setVotes(data.votes)
+        }
+      })
+    }
+  })
+})
