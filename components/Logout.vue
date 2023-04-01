64a0fe727b146e057c1790dbd45ea2af02b433f1
Larry W <l422y@l422y.com>
2023-03-31 23:28:43 -0400

    chore: preliminary logout, doesn't quite log you out from the browser, though.

---
+++ components/Logout.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="logout">
+    <button @click="logout">Logout</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const logout = () => {
+  localStorage.removeItem("spotify_access_token")
+  localStorage.removeItem("spotify_refresh_token")
+  localStorage.removeItem("spotify_auth_token")
+  window.location.href = "/"
+}
+</script>
+
+<style lang="scss">
+.logout {
+  position: fixed;
+  top: 0;
+  z-index: 999;
+  right: 0;
+  left: 0;
+  padding:1rem;
+  display: grid;
+  place-items: flex-end;
+
+  button {
+    background-color: #1db954;
+    color: #ffffff;
+    border: none;
+    padding: 1rem 2rem;
+    border-radius: 5px;
+    font-size: 1.5rem;
+    font-weight: 600;
+    cursor: pointer;
+    opacity: 0;
+    transition: opacity 0.2s ease-in-out;
+  }
+
+  &:hover {
+    button {
+      opacity: 1;
+    }
+  }
+
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,20 @@
 <template>
   <main id="app">
+    <Logout v-if="isAuthenticated"/>
     <NuxtPage/>
     <NowPlaying mode="normal"/>
   </main>
 </template>
 <script lang="ts" setup>
+import { useAppStore } from "~/stores/app"
 
+const appStore = useAppStore()
+const isAuthenticated = computed(() => appStore.$state.isAuthenticated)
 </script>
 
 <style lang="scss">
 main#app {
-  font-family: 'Roboto', "Helvetica Neue",sans-serif;
+  font-family: 'Roboto', "Helvetica Neue", sans-serif;
   font-size: 16px;
 }
 </style>
