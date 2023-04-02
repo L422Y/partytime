938ba8391264842f83f4f1fce2969c9a4b0b762d
Larry W <l422y@l422y.com>
2023-04-02 16:37:11 -0400

    chore: add debug component

---
+++ components/Queue/DebugQueue.vue
@@ -0,0 +1,14 @@
+<template>
+    <div style="font-size: 1rem">
+        <div v-for="track in currentQueueTracks" :key="track.id" class="queue__track">
+            VOTES {{ votesStore.$state.votesById[track.originalIndex+1]?.length || 0 }} -- IDX {{track.index}} -- OIDX
+            {{track.originalIndex}} -- {{track.track.name}}
+        </div>
+    </div>
+</template>
+<script lang="ts" setup>
+import { useVotesStore } from "~/stores/votes"
+
+const votesStore = useVotesStore()
+const currentQueueTracks = useState("currentQueueTracks", () => [])
+</script>
