897d4744f530d69d7f703f816d33955dda3cc258
Larry W <l422y@l422y.com>
2023-04-02 00:49:21 -0400

    chore: fix sorting, only update as needed

--- components/Queue/CurrentQueue.vue
+++ components/Queue/CurrentQueue.vue
@@ -1,52 +1,83 @@
 <template>
   <div class="queue">
     <header class="queue__head">
-      <span class="queue__upnext">UP NEXT</span>
-      <VotingInfo class="queue__voting" v-if="voting" />
+      <span class="queue__upnext"><span>
+        UP NEXT
+      </span> <Meter :value="meterConfig.value * 100" class="queue__meter" meterStyle="round"/></span>
+      <VotingInfo v-if="voting" class="queue__voting"/>
     </header>
     <div class="queue__items">
-      <QueuedTrack v-for="{index, originalIndex, track} in sortedTracks" :index="index" :originalIndex="originalIndex"
-                   :track="track"/>
+      <QueuedTrack v-for="queuedTrack in sortedTracks" :queued-track="queuedTrack"/>
     </div>
   </div>
-
 </template>
 <script lang="ts" setup>
 import { usePlayerStore } from "~/stores/player"
 import { computed } from "#imports"
 import QueuedTrack from "~/components/Queue/QueuedTrack.vue"
 import { useVotesStore } from "~/stores/votes"
-const tracks = computed(() => usePlayerStore().$state.currentQueue?.queue?.slice(0, 8) || [])
-const playerStore = usePlayerStore()
+import VotingInfo from "~/components/Voting/VotingInfo.vue"
+import Meter from "~/components/Meter.vue"
+
+const tracks = computed(() => usePlayerStore().$state.currentQueue?.queue?.slice(0, 12) || [])
 const votesStore = useVotesStore()
 
 const runtimeConfig = useRuntimeConfig()
 const voting = computed(() => runtimeConfig.public.voting)
+let currentId = 0
 
-const sortedTracks = computed(() => {
-  const indexedTracks = tracks.value.map((track, originalIndex) => ( {originalIndex, track} ))
-  let sortedTracks = indexedTracks.sort((a, b) => {
-    const aVotes = votesStore.$state.votesById[a.originalIndex + 1]?.length || 0
-    const bVotes = votesStore.$state.votesById[b.originalIndex + 1]?.length || 0
-    return bVotes - aVotes
-  })
+const meterConfig = ref({color: "#ffffff", value: 0})
+const indexedTracks = ref()
+const secondIndexed = ref()
 
-  sortedTracks = sortedTracks.map((track, index) => {
+const updateIndexedTracks = () => {
+  indexedTracks.value = tracks.value?.map((track, index) => {
     return {
-      index,
-      ...track
+      originalIndex: index,
+      track,
     }
   })
-  console.log("=====================")
+}
 
-  for (let i = 0; i < sortedTracks.length; i++) {
-    console.log(
-      sortedTracks[i].originalIndex,
-      votesStore.$state.votesById[sortedTracks[i].originalIndex]?.length || 0,
-      sortedTracks[i].index,
-      sortedTracks[i].track.name)
+watch(tracks, updateIndexedTracks)
+setTimeout(updateIndexedTracks, 0)
+let lastTracks: string = ""
+watch(indexedTracks, (tracks) => {
+  const json = JSON.stringify(tracks.map(({track}) => track.id))
+  if (lastTracks != "" && lastTracks === json) {
+    return
   }
-  return sortedTracks
+  lastTracks = json
+  secondIndexed.value = tracks.map(({originalIndex, track}, index) => {
+    return {
+      index,
+      originalIndex,
+      id: currentId++,
+      track: {
+        artists: track.artists,
+        name: track.name,
+        uri: track.uri,
+      },
+    }
+  })
+})
+
+const votesSnapshot = ref(votesStore.$state.votesById)
+let iteration = 0
+setInterval(() => {
+  if (iteration++ % 100 === 0) {
+    meterConfig.value.value = 0
+    votesSnapshot.value = votesStore.$state.votesById
+  }
+  meterConfig.value.value = ( iteration % 100 ) / 100
+}, 250)
+
+const sortedTracks = computed(() => {
+  return secondIndexed.value?.sort((a, b) => {
+    const aVotes = votesSnapshot.value[a.originalIndex]?.length || 0
+    const bVotes = votesSnapshot.value[b.originalIndex]?.length || 0
+    return bVotes - aVotes
+  }) || []
 })
 </script>
 <style lang="scss">
@@ -68,19 +99,25 @@ const sortedTracks = computed(() => {
     font-size: 3em;
     font-weight: 200;
     line-height: 1;
-    margin: 0 1.5em 1rem 1.5em;
-    text-transform: uppercase;
     display: flex;
     justify-content: space-between;
+    margin: 0 1.5em 1rem 1.5em;
+    text-transform: uppercase;
     @media (max-width: 768px) {
-      text-align: center;
-      flex-direction: column-reverse;
-      gap:2rem;
       font-size: 1.4rem;
+      flex-direction: column-reverse;
+      text-align: center;
+      gap: 2rem;
       .queue__upnext {
         font-size: 2.5rem;
       }
     }
+  }
+
+  .queue__upnext {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
   }
 
   &__items {
@@ -89,12 +126,29 @@ const sortedTracks = computed(() => {
     grid-template-columns: 1fr 1fr 1fr 1fr;
     @media (max-width: 1024px) {
       grid-template-columns: 1fr 1fr;
-
     }
     @media (orientation: portrait) and (max-width: 768px) {
       grid-template-columns: 1fr;
     }
     gap: 0.2rem;
   }
+
+  &__meter {
+
+    width: 2rem;
+    height: 2rem;
+    circle {
+      fill: none;
+      stroke-linecap: round;
+      &#track {
+        stroke: #ffffff22 !important;
+      }
+    }
+    @media (max-width: 768px) {
+      width: 1.5rem;
+      height: 1.5rem;
+    }
+  }
 }
+
 </style>
--- components/Queue/QueuedTrack.vue
+++ components/Queue/QueuedTrack.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="queued-track">
-    <div class="queued-track__index"><span>{{ trackIndex }}</span></div>
-    <div class="queued-track__info">
-      <div class="queued-track__meta">
-        <div class="queued-track__title" v-html="track.name"/>
-        <div class="queued-track__artist" v-html="artists"/>
-      </div>
-      <div class="queued-track__votes" v-if="voting">
-        <template v-if="votes">
-          {{ votes }} vote(s)
-        </template>
-        <template v-else>
-          No votes
-        </template>
+  <div class="queued-track" ref="outside">
+    <div class="queued-track__inside" ref="inside">
+      <div class="queued-track__index"><span>{{ trackIndex }}</span></div>
+      <div class="queued-track__info">
+        <div class="queued-track__meta">
+          <div class="queued-track__title" v-html="props.queuedTrack.track.name"/>
+          <div class="queued-track__artist" v-html="artists"/>
+        </div>
+        <div class="queued-track__votes" v-if="voting">
+          <template v-if="votes">
+            {{ votes }} vote(s)
+          </template>
+          <template v-else>
+            No votes
+          </template>
+        </div>
       </div>
     </div>
   </div>
@@ -20,27 +22,39 @@
 
 <script lang="ts" setup>
 import { computed } from "#imports"
-
-const {index, originalIndex, track} = defineProps<{
-  index: number,
-  originalIndex: number,
-  track: ISpotifyTrack
-}>()
-
 import { ISpotifyTrack } from "~/types/spotify"
 import { useVotesStore } from "~/stores/votes"
 
+const props = defineProps<{
+  queuedTrack: {
+    index: number,
+    id: number,
+    originalIndex: number,
+    track: ISpotifyTrack
+  }
+}>()
+
+const outside = ref()
+const inside = ref()
 const runtimeConfig = useRuntimeConfig()
 const voting = computed(() => runtimeConfig.public.voting)
-
-const trackIndex = originalIndex + 1
+const trackIndex = computed(() => props.queuedTrack.originalIndex + 1)
 const voteStore = useVotesStore()
 const votes = computed(() => {
-  return voteStore.$state.votesById[trackIndex]?.length
+  return voteStore.$state.votesById[props.queuedTrack.originalIndex]?.length
+})
+
+watch(props, () => {
+  if (props.queuedTrack.index) {
+    const containerBounds = outside.value.getBoundingClientRect()
+    inside.value.style.top = `${containerBounds.top}px`
+    inside.value.style.left = `${containerBounds.left}px`
+    inside.value.scrollIntoView({ behavior: "smooth" })
+  }
 })
 const artists = computed(() => {
-  if (track.artists) {
-    return track.artists
+  if (props.queuedTrack.track.artists) {
+    return props.queuedTrack.track.artists
       .map((artist) => artist.name)
       .join(", ")
   }
@@ -50,13 +64,26 @@ const artists = computed(() => {
 
 <style lang="scss">
 .queued-track {
-  display: grid;
-  align-items: flex-start;
-  padding: 1em;
-  cursor: pointer;
-  transition: all 0.2s ease-in-out;
-  background-color: #0009;
-  grid-template-columns: 4em 1fr;
+  //display: grid;
+  //align-items: flex-start;
+  //padding: 1em;
+  //cursor: pointer;
+  //transition: all 0.2s ease-in-out;
+  //background-color: #0001;
+  //grid-template-columns: 4em 1fr;
+  //height: 3rem;
+
+  &__inside {
+    display: grid;
+    align-items: flex-start;
+    padding: 1em;
+    cursor: pointer;
+    transition: all 2s ease-in-out;
+    background-color: #0009;
+    grid-template-columns: 4em 1fr;
+    //position: fixed;
+    //width: calc((100vw - 10em) / 4);
+  }
 
   &:hover {
     background-color: #000f;
